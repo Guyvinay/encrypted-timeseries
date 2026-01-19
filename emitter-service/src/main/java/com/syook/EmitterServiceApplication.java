bc059a8fbf96a0bbaf252606de4b8c0ca84cec0c
guyvinay <mrsinghvinay563@gmial.com>
2026-01-19 19:26:40 +0530

    emitter and listener service initialized

---
+++ emitter-service/src/main/java/com/syook/EmitterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.syook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmitterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmitterServiceApplication.class, args);
+	}
+
+}
---
+++ listener-service/src/main/java/com/syook/ListenerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.syook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListenerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListenerServiceApplication.class, args);
+	}
+
+}
