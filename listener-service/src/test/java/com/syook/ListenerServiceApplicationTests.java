86cdbc004e82c7c966f04fbec6567d2b11b36b86
guyvinay <mrsinghvinay563@gmial.com>
2026-01-20 02:10:47 +0530

    push docker-compose.yml with postgres and project Dockerfile

--- listener-service/src/test/java/com/syook/ListenerServiceApplicationTests.java
+++ listener-service/src/test/java/com/syook/ListenerServiceApplicationTests.java
@@ -1,12 +1,7 @@
 package com.syook;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class ListenerServiceApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
